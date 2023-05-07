74781bb5e778eb9a00ea038407da2e6bfdd7264b
tranngoc <tranngoc.contact@gmail.com>
2023-05-07 23:54:48 +0700

    u

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"runtime"
+	"sync"
+	"time"
+)
+
 type person struct {
 	name string
 }
 
 func main() {
-	m := make(map[int]person)
-	m[0] = person{
-		name: "TDN",
+	runtime.GOMAXPROCS(6)
+
+	now := time.Now()
+	wg := sync.WaitGroup{}
+	for i := 0; i < 20000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, err := http.Get("http://localhost:8080")
+			if err != nil {
+				log.Println(err)
+			}
+		}()
 	}
-	// m[0].name = "ABC" // viet tat
-	a := m[0]
-	a.name = "ABC"
+
+	wg.Wait()
+	log.Printf("since %fs\n", time.Since(now).Seconds())
 }
